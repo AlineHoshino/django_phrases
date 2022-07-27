2204f81904b458d82e1468aedccdf52c0efafde5
ALINE <hoshinoaline@yahoo.com.br>
2022-07-27 16:33:02 -0300

    feat: store and display user-data

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,7 @@
-from re import template
-from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -11,7 +10,7 @@ class NotesDeleteView(DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
-    
+
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
@@ -22,10 +21,20 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False) # cria o objeto, mas nao salva na database
+        self.object.user = self.request.user # add a user
+        self.object.save() # save the note with the user in the database
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = 'admin' # vai para a pagina admin
+
+    def get_queryset(self):
+        return self.request.user.notes.all()#pegar tudo só daquele usuario
 
 
 class NotesDetailView(DetailView):
