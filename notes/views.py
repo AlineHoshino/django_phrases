6ebea55e95bd96ce2aaa78fac73e93dfeeb2d503
ALINE <hoshinoaline@yahoo.com.br>
2022-07-28 09:45:27 -0300

    feat: add login, logout

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404, HttpResponseRedirect
+from django.http.response import HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,40 +6,44 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import NotesForm
 from .models import Notes
 
-class NotesDeleteView(DeleteView):
+
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = "/login"
 
-class NotesUpdateView(UpdateView):
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/login"
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/login"
 
     def form_valid(self, form):
-        self.object = form.save(commit=False) # cria o objeto, mas nao salva na database
-        self.object.user = self.request.user # add a user
-        self.object.save() # save the note with the user in the database
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
         return HttpResponseRedirect(self.get_success_url())
 
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
-    context_object_name = 'notes'
-    template_name = 'notes/notes_list.html'
-    login_url = 'admin' # vai para a pagina admin
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    login_url = "/login"
 
     def get_queryset(self):
-        return self.request.user.notes.all()#pegar tudo só daquele usuario
+        return self.request.user.notes.all()
 
-
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
-    context_object_name ='note'
+    context_object_name = "note"
+    login_url = "/login"
 
 # def list(request):
 #     all_notes = Notes.objects.all()
